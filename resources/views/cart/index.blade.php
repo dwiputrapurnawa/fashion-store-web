20fe4e762ac72ae7f9d22658286ed7ff33e165d7
Ida Bagus Dwi Putra Purnawa <putrapurnawa@gmail.com>
2023-09-26 06:08:21 +0800

    fix cart

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -6,64 +6,104 @@
 
 @section('content')
 
-@if (session()->has("message"))
-        <div class="alert alert-info alert-dismissible fade show" role="alert">
-            {{ session("message") }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>
+  @if (session()->has("message"))
+      <div class="alert alert-info alert-dismissible fade show" role="alert">
+          {{ session("message") }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
   @endif
 
-<div class="row">
+  @if ($products->isEmpty())
   
-    <div class="col-lg-8">
+      <div class="m-auto text-align-center" style="width: 25rem;">
+        <img class="img-fluid m-auto d-block" src="/images/empty-cart.png" alt="empty-cart" style="width: 10rem;">
+        <h2 class="mb-4 mt-3">Your Cart is <span class="fw-bold" style="color: #8F5E2E">Empty!</span></h2>
+        <a href="/" class="btn custom-btn w-100">Return To Shop</a>
+      </div>
+
+  @else
+  <div class="row">
+  
+    <div class="col-lg-auto">
       <h3>Cart</h3>
       <hr>
         @foreach ($products as $product)
-            <div class="card mb-3" id="card">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="https://www.diadora.com/dw/image/v2/BBPK_PRD/on/demandware.static/-/Sites-diadora-master/default/dw250e8493/images/hi-res/502.180038_50025_00_HR.jpg?sw=1920" class="img-fluid rounded-start" alt="...">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">{{ $product->name }}</h5>
-                      <input type="hidden" name="cart_id" value="{{ $product->pivot->id }}">
-                      <p class="card-text product-price currency">{{ $product->price }}</p>
-                      <div class="mb-3">
-                        <p class="d-inline">Total Stock: </p>
-                        <p class="stock d-inline">{{ $product->stock }}</p>
-                    </div>
-                      <div class="row">
-                        <div class="col-sm-auto">
-                          <div class="row mb-3 m-auto">
-                            <div class="col-sm-auto">
-                                <button class="btn custom-btn minus-cart-button" type="button"><i class="fa-solid fa-minus"></i></button>
-                            </div>
-                            <div class="col-sm-auto">
-                                <input class="form-control" type="number" name="quantity" min="1" value="{{ $product->pivot->quantity }}" max="{{ $product->stock }}" id="quantity">
-                            </div>
-                            <div class="col-sm-auto">
-                                <button class="btn custom-btn plus-cart-button" type="button"><i class="fa-solid fa-plus"></i></button>
-                            </div>
-                        </div>  
-                        </div>
+        <a class="text-decoration-none mb-3" href="/product/{{ $product->slug }}">
+          <div class="card mb-3" id="card" style="width: 50rem">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="https://www.diadora.com/dw/image/v2/BBPK_PRD/on/demandware.static/-/Sites-diadora-master/default/dw250e8493/images/hi-res/502.180038_50025_00_HR.jpg?sw=1920" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $product->name }}</h5>
+                  <input type="hidden" name="cart_id" value="{{ $product->pivot->id }}">
+                  <p class="card-text product-price currency fw-bold">{{ $product->price }}</p>
+                  <div class="row mb-3">
+                    <p class="d-inline col-sm-auto">Total Stock: </p>
+                    <p class="stock d-inline col-sm-auto">{{ $product->stock }}</p>
+                </div>
 
-                        <div class="col-sm">
-                          <button class="btn btn-danger" type="button" data-bs-toggle="modal" data-bs-target="#confirmDelete"><i class="fa-solid fa-trash"></i></button>
+                <div class="row mt-3">
+                  <div class="col-sm">
+                    <p class="fw-bold">Subtotal</p>
+                  </div>
+                  <div class="col-sm-auto">
+                    <p class="currency subtotal fw-bold">{{ $product->pivot->quantity * $product->price }}</p>
+                  </div>
+                </div>
+
+                  <div class="row mt-5">
+                    <div class="col-sm-auto">
+                      <div class="row mb-3 m-auto">
+                        <div class="col-sm-auto">
+                            <button class="btn custom-btn minus-cart-button" type="button"><i class="fa-solid fa-minus"></i></button>
                         </div>
-                        <div class="row mt-3 m-auto">
-                          <div class="col-sm">
-                            <p>Subtotal</p>
-                          </div>
-                          <div class="col-sm-auto">
-                            <p class="currency subtotal">{{ $product->pivot->quantity * $product->price }}</p>
-                          </div>
+                        <div class="col-sm-auto">
+                            <input class="form-control" type="number" name="quantity" min="1" value="{{ $product->pivot->quantity }}" max="{{ $product->stock }}" id="quantity">
                         </div>
-                      </div>
+                        <div class="col-sm-auto">
+                            <button class="btn custom-btn plus-cart-button" type="button"><i class="fa-solid fa-plus"></i></button>
+                        </div>
+                    </div>  
                     </div>
+
+                    <div class="col-sm">
+                      <button class="btn btn-danger float-end" type="button" data-bs-toggle="modal" data-bs-target="#confirmDelete"><i class="fa-solid fa-trash"></i></button>
+                    </div>
+                    
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </a>
+
+
+              <div class="modal fade" id="confirmDelete" tabindex="-1" aria-labelledby="confirmDelete" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Confirm Delete</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="/cart" method="POST">
+                      @csrf
+                      @method("delete")
+                      <input type="hidden" name="cart_id" value="{{ $product->pivot->id }}">
+                      <div class="modal-body">
+                        Are you sure delete this item from your cart?
+                      </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn custom-btn-outline" data-bs-dismiss="modal">No</button>
+                      <button type="submit" class="btn custom-btn">Yes</button>
+                    </div>
+              
+                  </form>
+                  </div>
+                </div>
+              </div>
+              
             @endforeach
         
     </div>
@@ -100,29 +140,5 @@
           </div>
     </div>
 </div>
-
-<div class="modal fade" id="confirmDelete" tabindex="-1" aria-labelledby="confirmDelete" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Confirm Delete</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <form action="/cart" method="POST">
-        @csrf
-        @method("delete")
-        <input type="hidden" name="cart_id" value="{{ $product->pivot->id }}">
-        <div class="modal-body">
-          Are you sure delete this item from your cart?
-        </div>
-      <div class="modal-footer">
-        <button type="button" class="btn custom-btn-outline" data-bs-dismiss="modal">No</button>
-        <button type="submit" class="btn custom-btn">Yes</button>
-      </div>
-
-    </form>
-    </div>
-  </div>
-</div>
-
+  @endif
 @endsection
